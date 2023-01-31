c568e2cf12fe15d6a8e629e25baee92e5f491c54
Bonyfus Martin <bonm@equinor.com>
2023-01-31 11:17:21 +0100

    initial commit

---
+++ src/Oplog/Oplog.Api/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Oplog.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddMicrosoftIdentityWebApiAuthentication(configuration);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(optionsA =>
+    {
+    }, optionsB =>
+    {
+
+        configuration.Bind("AzureAd", optionsB);
+        var defaultBackChannel = new HttpClient();
+        defaultBackChannel.DefaultRequestHeaders.Add("Origin", "oplog");
+        optionsB.Backchannel = defaultBackChannel;
+
+    })
+    .EnableTokenAcquisitionToCallDownstreamApi(e => { })
+    .AddInMemoryTokenCaches();
+
+SwaggerSetup.ConfigureServices(configuration, builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+SwaggerSetup.Configure(configuration, app);
+app.MapControllers();
+app.Run();
---
+++ src/Oplog/Oplog.Api/SwaggerSetup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Oplog.Api
+{
+    public static class SwaggerSetup
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = new Uri($"https://login.microsoftonline.com/{configuration["AzureAd:TenantId"]}/oauth2/v2.0/token"),
+                            AuthorizationUrl = new Uri($"https://login.microsoftonline.com/{configuration["AzureAd:TenantId"]}/oauth2/v2.0/authorize"),
+                            Scopes = new Dictionary<string, string> { { $"api://{configuration["AzureAd:ClientId"]}/User.Impersonation", "Oplog API" } },
+                        }
+                    },
+                    Description = "Oplog Security Scheme"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "OAuth2",
+                                    Type = ReferenceType.SecurityScheme
+                                } ,
+                                Scheme = "OAuth2",
+                                BearerFormat = "JWT",
+                                Type = SecuritySchemeType.Http,
+                                Name = "Bearer",
+                                In = ParameterLocation.Header
+                            },
+                            new List<string> { $"{configuration["AzureAd:ClientId"]}/User.Impersonation" }
+
+                        }
+                    });
+            });
+        }
+
+        public static void Configure(IConfiguration configuration, IApplicationBuilder app)
+        {           
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Oplog API");
+                c.OAuthClientId(configuration["AzureAd:ClientId"]);
+                c.OAuthAppName("Oplog");
+                c.OAuthScopeSeparator(" ");
+                c.OAuthUsePkce();
+            });
+        }
+    }
+}
