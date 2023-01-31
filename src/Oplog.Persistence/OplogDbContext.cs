330b99c5fbf098dbc22c96639e29b2a0a971c903
Bonyfus Martin <bonm@equinor.com>
2023-01-31 13:31:26 +0100

    feat: add initial db context

---
+++ src/Oplog.Persistence/OplogDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oplog.Persistence
+{
+    public class OplogDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public OplogDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
