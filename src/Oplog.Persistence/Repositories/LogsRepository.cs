b027859ab37553d388664a4068fe489e8f4176f0
FredrikMWold <fredrikmikalwold@gmail.com>
2023-06-07 09:24:36 +0200

    feat: critical logs within the to/from date is always returned (#47)

--- src/Oplog.Persistence/Repositories/LogsRepository.cs
+++ src/Oplog.Persistence/Repositories/LogsRepository.cs
@@ -47,14 +47,17 @@ namespace Oplog.Persistence.Repositories
         public async Task<List<LogsView>> GetFilteredLogs(int[] logTypeIds, int[] areaIds, int[] subTypeIds, int[] unitIds, string searchText, DateTime fromDate, DateTime toDate)
         {
             return await _dbContext.LogsView
-                       .Where(l => l.CreatedDate >= fromDate && l.CreatedDate <= toDate
-                       && (logTypeIds.Contains(l.LogTypeId.Value) || logTypeIds == null)
-                       && (areaIds.Contains(l.OperationAreaId.Value) || areaIds == null)
-                       && (subTypeIds.Contains(l.Subtype.Value) || subTypeIds == null)
-                       && (unitIds.Contains(l.Unit.Value) || unitIds == null)
-                       && (logTypeIds.Contains(l.LogTypeId.Value) || logTypeIds == null)
-                       && (l.Text.Contains(searchText) || string.IsNullOrWhiteSpace(searchText)))
-                       .OrderByDescending(l => l.CreatedDate).ToListAsync();
+                       .Where(l => 
+                       (l.CreatedDate >= fromDate && l.CreatedDate <= toDate) && ((
+                            (logTypeIds.Contains(l.LogTypeId.Value) || logTypeIds == null)
+                            && (areaIds.Contains(l.OperationAreaId.Value) || areaIds == null)
+                            && (subTypeIds.Contains(l.Subtype.Value) || subTypeIds == null)
+                            && (unitIds.Contains(l.Unit.Value) || unitIds == null)
+                            && (logTypeIds.Contains(l.LogTypeId.Value) || logTypeIds == null)
+                            && (l.Text.Contains(searchText) || string.IsNullOrWhiteSpace(searchText)))
+                            || (l.IsCritical == true)
+                        )
+                    ).OrderByDescending(l => l.CreatedDate).ToListAsync();
         }
 
     }
