e191edb454ae0a8e3797a5da38c153ae220f8368
Bonyfus Martin <bonm@equinor.com>
2023-07-26 09:36:57 +0200

    feat: only return active areas
    only return active areas

--- src/Oplog.Persistence/Migrations/20230726072829_AddIsActiveFieldForArea.Designer.cs
+++ src/Oplog.Persistence/Migrations/20230726072829_AddIsActiveFieldForArea.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oplog.Persistence;
 
@@ -11,9 +12,11 @@ using Oplog.Persistence;
 namespace Oplog.Persistence.Migrations
 {
     [DbContext(typeof(OplogDbContext))]
-    partial class OplogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230726072829_AddIsActiveFieldForArea")]
+    partial class AddIsActiveFieldForArea
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
