302a93742c8c3aa5b0fa0a569d030a198964d81c
bonyfusbouvet <97030288+bonyfusbouvet@users.noreply.github.com>
2023-09-06 08:47:03 +0200

    feat: adding more filters
    adding moe filters

--- src/Oplog.Core/AzureSearch/SearchOptionsBuilder.cs
+++ src/Oplog.Core/AzureSearch/SearchOptionsBuilder.cs
@@ -12,6 +12,7 @@ namespace Oplog.Core.AzureSearch
         private const string DateTimeFormat = @"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'";
         private readonly bool _isDateOnlySearch = false;
         private const string FilterPlaceHolderValue = "[]";
+        private const string LogTypeIdFieldName = "LogTypeId", AreaIdFieldName = "OperationAreaId", SubTypeIdFieldName = "Subtype", UnitIdFieldName = "Unit";
 
         public SearchOptionsBuilder(DateTime fromDate, DateTime toDate, int pageSize, int pageNumber, bool isDateOnlySearch)
         {
@@ -59,36 +60,34 @@ namespace Oplog.Core.AzureSearch
 
         public void AddLogTypeFilter(int[] logTypeIds)
         {
-            if (logTypeIds == null || logTypeIds.Count() < 1) return;
+            if (logTypeIds == null || logTypeIds.Length == 0) return;
 
-            StringBuilder stringBuilder = new();
-
-            foreach (int logTypeId in logTypeIds)
-            {
-                stringBuilder.Append(@$"LogTypeId eq {logTypeId.ToString()} or ");
-            }
-
-            string logTypeFilter = stringBuilder.ToString();
-
-            //Note: remove any trailing "or" operators
-            logTypeFilter = logTypeFilter.Remove(logTypeFilter.Length - 4);
-
+            string logTypeFilter = CreateFieldsFilter(logTypeIds, LogTypeIdFieldName);
             _fieldsFilter.Append($" and ({logTypeFilter})");
         }
 
         public void AddAreaFilter(int[] areaIds)
         {
-            if (areaIds == null) return;
+            if (areaIds == null || areaIds.Length == 0) return;
+
+            string logTypeFilter = CreateFieldsFilter(areaIds, AreaIdFieldName);
+            _fieldsFilter.Append($" and ({logTypeFilter})");
         }
 
         public void AddSubTypeFilter(int[] subTypeIds)
         {
-            if (subTypeIds == null) return;
+            if (subTypeIds == null || subTypeIds.Length == 0) return;
+
+            string logTypeFilter = CreateFieldsFilter(subTypeIds, SubTypeIdFieldName);
+            _fieldsFilter.Append($" and ({logTypeFilter})");
         }
 
         public void AddUnitFilter(int[] unitIds)
         {
-            if (unitIds == null) return;
+            if (unitIds == null || unitIds.Length == 0) return;
+
+            string logTypeFilter = CreateFieldsFilter(unitIds, UnitIdFieldName);
+            _fieldsFilter.Append($" and ({logTypeFilter})");
         }
 
         public SearchOptions Build()
@@ -99,15 +98,35 @@ namespace Oplog.Core.AzureSearch
             }
             else
             {
-                //Note: remove the "and" operator
-                _fieldsFilter = _fieldsFilter.Remove(0, 5);
+                if (_fieldsFilter.Length != 0)
+                {
+                    //Note: remove the "and" operator
+                    _fieldsFilter = _fieldsFilter.Remove(0, 5);
 
-                string filter = _filter.ToString();
-                filter = filter.Replace("[]", _fieldsFilter.ToString());
-                _searchOptions.Filter = filter;
+                    string filter = _filter.ToString();
+                    filter = filter.Replace("[]", _fieldsFilter.ToString());
+                    _searchOptions.Filter = filter;
+                }
             }
 
             return _searchOptions;
+        }
+
+        private string CreateFieldsFilter(int[] fieldIds, string fieldName)
+        {
+            StringBuilder stringBuilder = new();
+
+            foreach (int logTypeId in fieldIds)
+            {
+                stringBuilder.Append(@$"{fieldName} eq {logTypeId.ToString()} or ");
+            }
+
+            string query = stringBuilder.ToString();
+
+            //Note: remove any trailing "or" operators
+            query = query.Remove(query.Length - 4);
+
+            return query;
         }
     }
 }
